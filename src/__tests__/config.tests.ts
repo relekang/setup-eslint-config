0a07e5149abd93e9286c29b39577baccc9312ab8
Rolf Erik Lekang <me@rolflekang.com>
2022-01-22 22:09:53 +0100

    feat: Add parsing of version passed to npx
    npx is often running the locally installed version even if a version
    number is passed.

---
+++ src/__tests__/config.tests.ts
@@ -0,0 +1,62 @@
+import { createInstallConfigTask } from "../config";
+
+describe("createInstallConfigTask", () => {
+  it("should  with specific version", () => {
+    const task = createInstallConfigTask({
+      // @ts-expect-error test data
+      options: {},
+      // @ts-expect-error test data
+      setupConfig: {
+        packageInfo: { name: "eslint-config-party", version: "1.0.0" },
+      },
+      argv: ["npx", "eslint-config-party@4.2.0"],
+      env: { npm_lifecycle_event: "npx" },
+    });
+    expect(task.title).toEqual("Installing eslint-config-party@4.2.0");
+    expect(task.skip).toEqual(false);
+  });
+
+  it("should create task with latest version", () => {
+    const task = createInstallConfigTask({
+      // @ts-expect-error test data
+      options: {},
+      // @ts-expect-error test data
+      setupConfig: {
+        packageInfo: { name: "eslint-config-party", version: "1.0.0" },
+      },
+      argv: ["npx", "eslint-config-party@latest"],
+      env: { npm_lifecycle_event: "npx" },
+    });
+    expect(task.title).toEqual("Installing eslint-config-party@latest");
+    expect(task.skip).toEqual(false);
+  });
+
+  it("should create task without version", () => {
+    const task = createInstallConfigTask({
+      // @ts-expect-error test data
+      options: {},
+      // @ts-expect-error test data
+      setupConfig: {
+        packageInfo: { name: "eslint-config-party", version: "1.0.0" },
+      },
+      argv: ["npx", "eslint-config-party"],
+      env: { npm_lifecycle_event: "npx" },
+    });
+    expect(task.title).toEqual("Installing eslint-config-party@1.0.0");
+    expect(task.skip).toEqual(false);
+  });
+
+  it("should create skip task", () => {
+    const task = createInstallConfigTask({
+      // @ts-expect-error test data
+      options: {},
+      // @ts-expect-error test data
+      setupConfig: {
+        packageInfo: { name: "eslint-config-party", version: "1.0.0" },
+      },
+      argv: ["npx", "eslint-config-party"],
+      env: {},
+    });
+    expect(task.skip).toEqual(true);
+  });
+});
